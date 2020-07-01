ced482a661a9fab76fe8f5f644a3fa9828e2a955
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-06-30 22:27:04 -0400

    added cdn and removed script links

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -179,8 +179,8 @@
 </div>
 
 <!-- Scripts -->
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
     <script src="js/main.js"></script>
