7932a1d20981e080f729cfada54cf30b98d73cf8
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-07-04 14:20:23 -0400

    added new schema

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -32,20 +32,46 @@
     <!-- Schema -->
     <script type="application/ld+json" async defer>
         {
-          "@context" : "http://schema.org",
-          "@type" : "LocalBusiness",
-          "name" : "RizzardiConstruction",
-          "telephone" : "+1 (814) 688-0382",
-          "email" : "patrick.d.rizzardi@gmail.com",
-          "address" : {
-            "@type" : "PostalAddress",
-            "streetAddress" : "2027 W 25th St.",
-            "addressLocality" : "Erie,",
-            "addressRegion" : "PA",
-            "addressCountry" : "United States"
-          },
-          "image" : [ "https://rizzardiconstruction.com/images/myPics/kitchen.jpg", "https://rizzardiconstruction.com/images/myPics/bathroom.jpg", "https://rizzardiconstruction.com/images/myPics/house.jpg", "https://rizzardiconstruction.com/images/myPics/deck.jpg", "https://rizzardiconstruction.com/images/myPics/siding.jpg", "https://rizzardiconstruction.com/images/hero_1.jpg" ]
-        }
+            {
+                "@context": "https://schema.org",
+                "@type": "GeneralContractor",
+                "name": "Rizzardi Construction",
+                "image": "",
+                "@id": "",
+                "url": "https://rizzardiconstruction.com",
+                "telephone": "( 814)688-0382",
+                "priceRange": "5,000 - 250,000",
+                "address": {
+                  "@type": "PostalAddress",
+                  "streetAddress": "2027 West 25th Street",
+                  "addressLocality": "Erie",
+                  "addressRegion": "PA",
+                  "postalCode": "16502",
+                  "addressCountry": "US"
+                },
+                "geo": {
+                  "@type": "GeoCoordinates",
+                  "latitude": 42.0959114,
+                  "longitude": -80.11932589999999
+                },
+                "openingHoursSpecification": {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": [
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday"
+                  ],
+                  "opens": "08:00",
+                  "closes": "14:30"
+                },
+                "image" : [ "https://rizzardiconstruction.com/images/myPics/kitchen.webp", 
+                "https://rizzardiconstruction.com/images/myPics/bathroom.webp", 
+                "https://rizzardiconstruction.com/images/myPics/house.webp", 
+                "https://rizzardiconstruction.com/images/myPics/deck-service.webp", 
+                "https://rizzardiconstruction.com/images/myPics/window-service.webp" ]
+              }
         </script>
     <!-- End of Schema -->
 </head>
